b0bae96736ea6e95033fc6964f9f478b52b120a6
Андрей Рябинов <andryabinov@gmail.com>
2020-10-23 00:44:38 +0300

    add make_game

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+
+import random
+
+
+def make_game():
+    qestion_number = random.randrange(1000)
+    if qestion_number % 2 == 0:
+        qestion_answer = 'yes'
+    else:
+        qestion_answer = 'no'
+    return (qestion_number , qestion_answer)
+
+
+def main():
+    make_game()
+
+
+if __name__ == '__main__':
+    main()
