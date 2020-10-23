9951ead01c68d2bec6c2e877bc88f5fa41eba6aa
Андрей Рябинов <andryabinov@gmail.com>
2020-10-23 20:43:57 +0300

    user_name fixes

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -4,32 +4,38 @@
 import prompt, random
 
 
-#dialog's strings
-game_rules_string = 'Answer "yes" if the number is even, otherwise answer "no".'
-win_message = 'Correct!'
-loose_message = "is wrong answer ;(. Correct answer was 'no'.\nLet's try again, Bill!"
-
-
-#game settings
-rounds_to_win = 3
-
 def welcome_user():
+
+
     """GET user name and say HI"""
+
+    
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print('Hello, ' + name + '!')
     return name
 
 def make_game():
+
+
     qestion_number = random.randrange(1000)
+
+
     if qestion_number % 2 == 0:
         qestion_answer = 'yes'
     else:
         qestion_answer = 'no'
     return (str(qestion_number) , qestion_answer)
 
-def game_iteration(game):
+def game_iteration(game, user_name):
+
+
     (qestion, answer) = game
+    game_rules_string = 'Answer "yes" if the number is even, otherwise answer "no".'
+    win_message = 'Correct!'
+    loose_message = "is wrong answer ;(. Correct answer was 'no'.\nLet's try again, " + user_name + "!"
+
+
     print(game_rules_string)
     print('Qestion:',qestion)
     user_answer = prompt.string('Your answer: ')
@@ -43,10 +49,14 @@ def game_iteration(game):
 
 
 def main():
+
+
+    user_name = welcome_user()
     rounds_to_win = 3
-    #welcome_user()
+
+
     while rounds_to_win > 0:
-        game = game_iteration(make_game())
+        game = game_iteration(make_game(), user_name)
         if  game == False:
             return False
         else:
