b5f9e847b6f3d7932559bd710024ea16bb9d87c8
andrejrabinov <andryabinov@gmail.com>
2020-12-06 18:49:40 +0300

    move all logic to game & game runner

---
+++ brain_games/run/game_runner.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import random
+import prompt
+from brain_games.run.welcome_user import welcome_user
+
+def game_iteration(game, user_name):
+    (question, answer) = game
+    game_rules_string = 'Answer "yes" if the number is even, otherwise answer "no".'
+    win_message = 'Correct!'
+    loose_message = "is wrong answer ;(. Correct answer was \"" + answer + "\".\nLet's try again, " + user_name + "!"
+
+    print(game_rules_string)
+    print('Question:', question)
+    user_answer = prompt.string('Your answer: ')
+    if user_answer == answer:
+        print(win_message)
+        return True
+    else:
+        print("\"" + user_answer + "\"", loose_message)
+        return False
+
+def runner(game_maker):
+    user_name = welcome_user()
+    ROUNDS_TO_WIN = 3
+    rounds_left = 0
+
+    while rounds_left < ROUNDS_TO_WIN:
+        game = game_iteration(game_maker(random.randrange(1000)), user_name)
+        if game:
+            rounds_left += rounds_left
+        else:
+            return False
+    return True
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -5,36 +5,12 @@ import prompt
 import random
 from brain_games.games.game_even import make_game
 from brain_games.run.welcome_user import welcome_user
-
-
-def game_iteration(game, user_name):
-    (question, answer) = game
-    game_rules_string = 'Answer "yes" if the number is even, otherwise answer "no".'
-    win_message = 'Correct!'
-    loose_message = "is wrong answer ;(. Correct answer was \"" + answer + "\".\nLet's try again, " + user_name + "!"
-
-    print(game_rules_string)
-    print('Question:', question)
-    user_answer = prompt.string('Your answer: ')
-    if user_answer == answer:
-        print(win_message)
-        return True
-    else:
-        print("\"" + user_answer + "\"", loose_message)
-        return False
+from brain_games.run.game_runner import runner
+from brain_games.run.game_runner import game_iteration
 
 
 def main():
-    user_name = welcome_user()
-    rounds_to_win = 3
-
-    while rounds_to_win > 0:
-        game = game_iteration(make_game(random.randrange(1000)), user_name)
-        if game:
-            rounds_to_win = rounds_to_win - 1
-        else:
-            return False
-    return True
+    runner(make_game)
 
 
 if __name__ == '__main__':
