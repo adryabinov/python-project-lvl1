f9494bbea47f4c2a9505f8009815c337f6483fe6
andrejrabinov <andryabinov@gmail.com>
2020-12-09 17:03:38 +0300

    todo fix

--- brain_games/games/game_gcd.py
+++ brain_games/games/game_gcd.py
@@ -4,7 +4,7 @@ import random
 
 def make_game(seed=random.random()):
     """seed = random return: TODO: game rule, qestion string, answer,"""
-    game_rules = "even rule: TODO REWRITE"
+    game_rules = "gcd rule: TODO REWRITE"
     question_number = 0
     question_answer = 0
     return str(question_number), str(question_answer), str(game_rules)
