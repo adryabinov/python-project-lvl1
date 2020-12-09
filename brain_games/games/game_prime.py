5d32fda771f3cae0b7058400ae0ce80c62f11cc1
andrejrabinov <andryabinov@gmail.com>
2020-12-09 17:46:19 +0300

    add prime logic

--- brain_games/games/game_prime.py
+++ brain_games/games/game_prime.py
@@ -4,7 +4,13 @@ import random
 
 def make_game(seed=random.random()):
     """seed = random return: TODO: game rule, qestion string, answer,"""
+    PRIME_BEFORE_100 = (1, 2, 3, 5, 7, 11, 13, 17, 73, 79, 83, 89, 97)
     game_rules = "prime rule: TODO REWRITE"
-    question_number = 0
-    question_answer = 0
-    return str(question_number), str(question_answer), str(game_rules)
+    question = int(seed * 100)
+
+    if PRIME_BEFORE_100.contain(question):
+        answer = "yes"
+    else:
+        answer = "no"
+    
+    return str(question), str(answer), str(game_rules)
