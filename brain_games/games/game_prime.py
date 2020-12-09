1f19e5d2837e96477f8fd93e5b028a5fa2653e4e
andrejrabinov <andryabinov@gmail.com>
2020-12-09 18:06:19 +0300

    fix logic

--- brain_games/games/game_prime.py
+++ brain_games/games/game_prime.py
@@ -4,11 +4,11 @@ import random
 
 def make_game(seed=random.random()):
     """seed = random return: TODO: game rule, qestion string, answer,"""
-    PRIME_BEFORE_100 = (1, 2, 3, 5, 7, 11, 13, 17, 73, 79, 83, 89, 97)
+    PRIME_BEFORE_100 = {1, 2, 3, 5, 7, 11, 13, 17, 73, 79, 83, 89, 97}
     game_rules = "prime rule: TODO REWRITE"
-    question = int(seed * 100)
+    question = int(seed * 10)
 
-    if PRIME_BEFORE_100.contain(question):
+    if question in PRIME_BEFORE_100:
         answer = "yes"
     else:
         answer = "no"
