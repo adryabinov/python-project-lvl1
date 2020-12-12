171324a2f885951081a58689c04afe701f2c6516
andrejrabinov <andryabinov@gmail.com>
2020-12-12 16:05:13 +0300

    fix typo

--- brain_games/games/game_progression.py
+++ brain_games/games/game_progression.py
@@ -7,13 +7,13 @@ def make_game(seed=random.random()):
     """seed = random return: TODO: qestion output format, game rule"""
     first_item = int(seed * 10)
     step = int((seed * 100) % 10)
-    qestion_item_position = int((seed * 1000) % 10)
+    question_item_position = int((seed * 1000) % 10)
     progression = []
 
     for i in range(10):
         progression.append(first_item + (step * i))
         
-    answer = progression[qestion_item_position]
+    answer = progression[question_item_position]
     progression[qestion_item_position] = '...'
     question = ' '.join([str(elem) for elem in progression])
 
