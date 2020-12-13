694e8ea361c3ea2afd58b80276d90d0ec5b42506
andrejrabinov <andryabinov@gmail.com>
2020-12-13 17:59:28 +0300

    fix logic

--- brain_games/games/game_gcd.py
+++ brain_games/games/game_gcd.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python
 import random
+from math import gcd
 
-PRIME_11 = [1, 2, 3, 5, 7, 11, 13, 17, 73, 79]
 GAME_RULE = 'Find the greatest common divisor of given numbers.'
 
 def make_game(seed=random.random()):
-    first_pos = int(seed * 10)
-    second_pos = int((seed * 100) % 10)
+    first_num = int(seed * 100)
+    second_num = int((seed * 10000) % 100)
+    question = str.format('{} {}', first_num, second_num)
+    answer = gcd(first_num, second_num)
 
-    if first_pos == second_pos & first_pos: #for diff numbers
-        second_pos += 1
-
-    num = int((seed * 1000) % 10) + 1 #1-10
-    question = str.format('{} {}', num * PRIME_11[first_pos], num * PRIME_11[second_pos])
-    answer = num
     return str(question), str(answer)
