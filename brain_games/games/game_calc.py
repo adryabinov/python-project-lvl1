ffa966bed2aecd69cc2203d47009340f736cfa9c
andrejrabinov <andryabinov@gmail.com>
2020-12-18 22:19:51 +0300

    lint fixes

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -3,10 +3,12 @@ from brain_games.engine import run
 
 
 DESCRIPTION = 'What is the result of the expression?'
-OPERATORS_FUNC = {'+': lambda x,y: x + y,
-            '-': lambda x,y: x - y, 
-            '*': lambda x,y: x * y}
-OPERATORS_LIST = list(OPERATORS_FUNC.keys())
+OPERATORS_FUNCTS = {
+    '+': lambda x, y: x + y,
+    '-': lambda x, y: x - y,
+    '*': lambda x, y: x * y,
+}
+OPERATORS_LIST = list(OPERATORS_FUNCTS.keys())
 
 
 def generate_round():
@@ -14,7 +16,7 @@ def generate_round():
     second_number = random.randint(1, 10)
     operator = random.choice(OPERATORS_LIST)
     question = (F'{first_number} {operator} {second_number}')
-    answer = str(OPERATORS_FUNC[operator](first_number, second_number))
+    answer = str(OPERATORS_FUNCTS[operator](first_number, second_number))
     return question, answer
 
 
