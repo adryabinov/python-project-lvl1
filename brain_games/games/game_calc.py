b4a60f179008f21f9402a7a538e29f43ae3326fa
andrejrabinov <andryabinov@gmail.com>
2020-12-18 22:22:57 +0300

    style fix

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -3,12 +3,12 @@ from brain_games.engine import run
 
 
 DESCRIPTION = 'What is the result of the expression?'
-OPERATORS_FUNCTS = {
+OPERATIONS = {
     '+': lambda x, y: x + y,
     '-': lambda x, y: x - y,
     '*': lambda x, y: x * y,
 }
-OPERATORS_LIST = list(OPERATORS_FUNCTS.keys())
+OPERATORS_LIST = list(OPERATIONS.keys())
 
 
 def generate_round():
@@ -16,7 +16,7 @@ def generate_round():
     second_number = random.randint(1, 10)
     operator = random.choice(OPERATORS_LIST)
     question = (F'{first_number} {operator} {second_number}')
-    answer = str(OPERATORS_FUNCTS[operator](first_number, second_number))
+    answer = str(OPERATIONS[operator](first_number, second_number))
     return question, answer
 
 
