8a35a7c715dcc7cc53b7762231f93809bdc62ebe
andrejrabinov <andryabinov@gmail.com>
2020-12-18 22:16:51 +0300

    add operators dict

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -3,24 +3,18 @@ from brain_games.engine import run
 
 
 DESCRIPTION = 'What is the result of the expression?'
-OPERATORS = ['+', '-', '*']
-
-
-def calculate(first_int, second_int, operator):
-    if operator == '+':
-        return first_int + second_int
-    elif operator == '-':
-        return first_int - second_int
-    else:
-        return first_int * second_int
+OPERATORS_FUNC = {'+': lambda x,y: x + y,
+            '-': lambda x,y: x - y, 
+            '*': lambda x,y: x * y}
+OPERATORS_LIST = list(OPERATORS_FUNC.keys())
 
 
 def generate_round():
-    first_number = random.randint(1, 100)
-    second_number = random.randint(1, 100)
-    operator = random.choice(OPERATORS)
+    first_number = random.randint(1, 10)
+    second_number = random.randint(1, 10)
+    operator = random.choice(OPERATORS_LIST)
     question = (F'{first_number} {operator} {second_number}')
-    answer = str(calculate(first_number, second_number, operator))
+    answer = str(OPERATORS_FUNC[operator](first_number, second_number))
     return question, answer
 
 
