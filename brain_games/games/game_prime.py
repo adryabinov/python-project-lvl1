1dc5a37cb3539600132a5480778b0127d55d91b3
andrejrabinov <andryabinov@gmail.com>
2020-12-18 22:38:50 +0300

    refact

--- brain_games/games/game_prime.py
+++ brain_games/games/game_prime.py
@@ -6,10 +6,11 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(number):
-    if number > 1:
-        for i in range(2, number):
-            if (number % i) == 0:
-                return False
+    if number == 1:
+        return True           
+    for i in range(2, number):
+        if (number % i == 0):
+            return False
     return True
 
 
