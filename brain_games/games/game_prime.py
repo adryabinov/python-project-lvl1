0a2657ea73545a0bea409c8deb675efe0ee86b0b
andrejrabinov <andryabinov@gmail.com>
2020-12-18 22:41:08 +0300

    lint

--- brain_games/games/game_prime.py
+++ brain_games/games/game_prime.py
@@ -7,7 +7,7 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def is_prime(number):
     if number == 1:
-        return True           
+        return True
     for i in range(2, number):
         if (number % i == 0):
             return False
