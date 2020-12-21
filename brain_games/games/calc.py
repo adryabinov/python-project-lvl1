1fb06c49564f591b0414a35310f1178ead55f4cd
andrejrabinov <andryabinov@gmail.com>
2020-12-21 19:17:20 +0300

    const fix

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -3,7 +3,7 @@ from brain_games.engine import run
 
 
 DESCRIPTION = 'What is the result of the expression?'
-MAP_OPERATORS_TO_OPERATIONS = {
+map_operators_to_operations = {
     '+': lambda x, y: x + y,
     '-': lambda x, y: x - y,
     '*': lambda x, y: x * y,
@@ -13,9 +13,9 @@ MAP_OPERATORS_TO_OPERATIONS = {
 def generate_round():
     first_number = random.randint(1, 10)
     second_number = random.randint(1, 10)
-    operator = random.choice(list(MAP_OPERATORS_TO_OPERATIONS.keys()))
+    operator = random.choice(list(map_operators_to_operations.keys()))
     question = F'{first_number} {operator} {second_number}'
-    answer = MAP_OPERATORS_TO_OPERATIONS[operator](
+    answer = map_operators_to_operations[operator](
         first_number,
         second_number)
     return question, str(answer)
