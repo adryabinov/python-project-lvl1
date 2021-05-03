c15a33aa799fec0905f426d06aed17f0e154490f
andrejrabinov <andryabinov@gmail.com>
2021-05-03 16:52:02 +0300

    remove redundant ()

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -9,7 +9,7 @@ def is_prime(number):
     if number <= 1:
         return False
     for i in range(2, number):
-        if (number % i == 0):
+        if number % i == 0:
             return False
     return True
 
